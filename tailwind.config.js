e6df0e3aa2037ccba345c3950763381857a0898b
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2022-10-03 15:15:14 +0200

    ft(desing): main pages design
    designs blog post page
    design home page
    design product page
    designs pricing page
    design contact page
    design blog page
    
    #done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}","./page/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
