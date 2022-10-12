e6e643a15e474546db3173ba5f815c03660f45d2
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2022-10-12 06:34:34 +0200

    refine(arbitrary): refin html and css
    Refin homepage
    refine product page
    refine about us
    
    #done

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,21 @@ module.exports = {
         },
         "light-violet": "#E0E4FC",
         "light-pink": "rgba(215, 152, 225, 0.15)",
-        "main" : "#F0F2FE"
+        "main" : "#F0F2FE",
+        "gradient-blue": "#373FFF",
+        "gradient-cyan": "#3ACAF8"
       },
       backgroundImage: {
-        footerGradient: "url('../assets/image/gradient-footer.png')"
+        footerGradient: "url('../assets/image/gradient-footer.png')",
+        headerDescriptionGradient: "url('../assets/svgs/aboutus-gradient.svg')"
+      },
+      fontSize: {
+        "size-small": "12px",
+        "size-medium": "16px",
+        "size-large": "18px",
+        "size-2large": "24px",
+        "size-3large": "48px",
+        "size-xlarge":"56px"
       }
     },
   },
